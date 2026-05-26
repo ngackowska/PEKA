9d65329f62614d9fc2ef2e6f47a340db5abe1e41
ngackowska <ngackowska02@gmail.com>
2026-05-26 15:59:11 +0200

    DepartureCard.kt - dodanie ikonek i reorganizacja układu

--- app/src/main/java/com/example/peka/modules/DepartureCard.kt
+++ app/src/main/java/com/example/peka/modules/DepartureCard.kt
@@ -17,12 +17,21 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.peka.api.TimeData
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.platform.LocalContext
+import com.example.peka.R
+import androidx.compose.ui.res.painterResource
 
 // Komponent wyświetlający informacje o danym odjeździe pojazdu
 // (pojedynczy odjazd danej linii widoczny np. po kliknięciu w kafelek na dashboard)
 
 @Composable
 fun DepartureCard(timeData: TimeData) {
+    val context = LocalContext.current
+
     Card(
         modifier = Modifier.fillMaxWidth(),
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
@@ -30,38 +39,19 @@ fun DepartureCard(timeData: TimeData) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
+                .padding(16.dp, 16.dp, 16.dp, 8.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
             Column {
-                Text(text = "Linia: ${timeData.line}", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+                Text(
+                    text = "Linia: ${timeData.line}",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                )
                 Spacer(modifier = Modifier.height(4.dp))
                 Text(text = "Kierunek: ${timeData.direction}")
 
-                // Tu jakieś ikonki pewnie apbo krótko
-                // na razie kilka przykładowych żeby zobaczyć co to (lowEntrance vs lowFloor nie wiem które to xD)
-                // do porównania z PEKA
-
-                if (timeData.airCnd == true) {
-                    Text(text = "Klimatyzacja: Tak", fontSize = 12.sp)
-                }
-
-                if (timeData.bike == true) {
-                    Text(text = "Rower: Tak", fontSize = 12.sp)
-                }
-
-                if (timeData.lowEntranceBus == true) {
-                    Text(text = "lowEntranceBus: Tak", fontSize = 12.sp)
-                }
-
-                if (timeData.lowFloorBus == true) {
-                    Text(text = "lowFloorBus: Tak", fontSize = 12.sp)
-                }
-
-                if (timeData.ticketMachine == true) {
-                    Text(text = "ticketMachine: Tak", fontSize = 12.sp)
-                }
 
 
             }
@@ -71,6 +61,140 @@ fun DepartureCard(timeData: TimeData) {
                 fontSize = 20.sp
             )
         }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp, 0.dp, 16.dp, 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Column() {
+
+                if (timeData.ticketMachine == true) {
+                    Text(text = "Dostępny biletomat.", fontSize = 12.sp)
+                }
+
+                if (timeData.vehicle != null) {
+                    Text(text = "Numer pojazdu: ${timeData.vehicle}", fontSize = 12.sp)
+                }
+
+                if (timeData.departure != null) {
+                    Text(text = "Odjazd: ${timeData.departure.substringAfter("T").take(5)}", fontSize = 12.sp)
+                }
+            }
+
+            // Tu jakieś ikonki pewnie apbo krótko
+            // na razie kilka przykładowych żeby zobaczyć co to (lowEntrance vs lowFloor nie wiem które to xD)
+            // do porównania z PEKA
+
+            Row {
+                if (timeData.bike == true) {
+                    Image(
+                        painter = painterResource(id = R.drawable.bike),
+                        contentDescription = "Możliwość przewozu rowerów.",
+                        modifier = Modifier
+                            .size(32.dp) // Ustawienie rozmiaru obrazka
+                            .clickable {
+                                // Wyświetlenie dymku z tekstem po kliknięciu
+                                Toast.makeText(context, "Możliwość przewozu rowerów.", Toast.LENGTH_SHORT).show()
+                            }
+                    )
+                }
+
+
+
+                if (timeData.airCnd == true) {
+                    Image(
+                        painter = painterResource(id = R.drawable.snow),
+                        contentDescription = "Pojazd wyposażony w klimatyzację.",
+                        modifier = Modifier
+                            .size(32.dp) // Ustawienie rozmiaru obrazka
+                            .padding(start = 3.dp)
+                            .clickable {
+                                // Wyświetlenie dymku z tekstem po kliknięciu
+                                Toast.makeText(context, "Pojazd wyposażony w klimatyzację.", Toast.LENGTH_SHORT).show()
+                            }
+                    )
+                }
+
+                if (timeData.charger == true) {
+                    Image(
+                        painter = painterResource(id = R.drawable.usb),
+                        contentDescription = "Możliwość ładowania gniazdem USB.",
+                        modifier = Modifier
+                            .size(32.dp) // Ustawienie rozmiaru obrazka
+                            .padding(start = 3.dp)
+                            .clickable {
+                                // Wyświetlenie dymku z tekstem po kliknięciu
+                                Toast.makeText(context, "Możliwość ładowania gniazdem USB.", Toast.LENGTH_SHORT).show()
+                            }
+                    )
+                }
+
+                if (timeData.lowEntranceBus == true) {
+                    Image(
+                        painter = painterResource(id = R.drawable.wheel_star),
+                        contentDescription = "Kurs obsługiwany pojazdem z niską podłogą w środkowym członie.",
+                        modifier = Modifier
+                            .size(32.dp) // Ustawienie rozmiaru obrazka
+                            .padding(start = 3.dp)
+                            .clickable {
+                                // Wyświetlenie dymku z tekstem po kliknięciu
+                                Toast.makeText(context, "Kurs obsługiwany pojazdem z niską podłogą w środkowym członie.", Toast.LENGTH_SHORT).show()
+                            }
+                    )
+                }
+
+                if (timeData.lowFloorBus == true) {
+                    Image(
+                        painter = painterResource(id = R.drawable.wheel),
+                        contentDescription = "Kurs obsługiwany pojazdem niskopodłogowym.",
+                        modifier = Modifier
+                            .size(32.dp) // Ustawienie rozmiaru obrazka
+                            .padding(start = 3.dp)
+                            .clickable {
+                                // Wyświetlenie dymku z tekstem po kliknięciu
+                                Toast.makeText(context, "Kurs obsługiwany pojazdem niskopodłogowym.", Toast.LENGTH_SHORT).show()
+                            }
+                    )
+                }
+
+                if (timeData.lfRamp == true) {
+                    Image(
+                        painter = painterResource(id = R.drawable.wheel_ramp),
+                        contentDescription = "Kurs obsługiwany pojazdem niskopodłogowym z rampą.",
+                        modifier = Modifier
+                            .size(32.dp) // Ustawienie rozmiaru obrazka
+                            .padding(start = 3.dp)
+                            .clickable {
+                                // Wyświetlenie dymku z tekstem po kliknięciu
+                                Toast.makeText(context, "Kurs obsługiwany pojazdem niskopodłogowym z rampą.", Toast.LENGTH_SHORT).show()
+                            }
+                    )
+                }
+
+                if (timeData.leRamp == true) {
+                    Image(
+                        painter = painterResource(id = R.drawable.wheel_ramp_star),
+                        contentDescription = "Kurs obsługiwany pojazdem z niską podłogą w środkowym członie z rampą.",
+                        modifier = Modifier
+                            .size(32.dp) // Ustawienie rozmiaru obrazka
+                            .padding(start = 3.dp)
+                            .clickable {
+                                // Wyświetlenie dymku z tekstem po kliknięciu
+                                Toast.makeText(context, "Kurs obsługiwany pojazdem z niską podłogą w środkowym członie z rampą.", Toast.LENGTH_SHORT).show()
+                            }
+                    )
+                }
+
+
+            }
+
+        }
+
+
     }
 }
 
